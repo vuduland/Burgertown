0b0af0f97c9aae6446ede572d198714f597f4d14
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-12 16:01:29 -0600

    server and orm begin setup

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,83 @@
 import { connection as db } from './connection';
 const mysql = require('mysql');
 
+// Here is the O.R.M. where you write functions that takes inputs and conditions
+// and turns them into database commands like SQL.
+
+function printQuestionMarks(num) {
+  const arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  // column1=value, column2=value2,...
+  const arr = [];
+
+  for (var key in ob) {
+    arr.push(key + '=' + ob[key]);
+  }
+
+  return arr.toString();
+}
+
+const db = {
+  all: function(tableInput, cb) {
+    const queryString = 'SELECT * FROM ' + tableInput + ';';
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // vals is an array of values that we want to save to cols
+  // cols are the columns we want to insert the values into
+  create: function(table, cols, vals, cb) {
+    const queryString = 'INSERT INTO ' + table;
+
+    queryString += ' (';
+    queryString += cols.toString();
+    queryString += ') ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(vals.length);
+    queryString += ') ';
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // objColVals would be the columns and values that you want to update
+  // an example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    const queryString = 'UPDATE ' + table;
+
+    queryString += ' SET ';
+    queryString += objToSql(objColVals);
+    queryString += ' WHERE ';
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+
+export const db = db;
+
 // example from online, but makes connection inside it
 // class User {
 //   static CreateTable() {
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse application body
+app.use(express.urlencoded({ extended: true, useUnifiedTopology: true }));
+app.use(express.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgersController.js');
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log('Listening on port:%s', PORT);
+});
