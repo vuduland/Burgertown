d8e4b3cfb34d850a250bea7e4b5928c99ed8c4f3
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-14 01:50:30 -0600

    finished

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: 'burgers_db',
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-//   },
-//   production: {
-//     use_env_variable: 'JAWSDB_URL',
-//     dialect: 'mysql'
-//   }
-// };
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'burgers_db',
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql'
+  }
+};
