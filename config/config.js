fd3e2bca5d0aa6e03a18514e88861a2b6aea0011
Victor <53414263+vuduland@users.noreply.github.com>
2020-02-14 02:03:11 -0600

    config and connection with jawsdb_url

--- config/config.js
+++ config/config.js
@@ -1,13 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'burgers_db',
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  },
   production: {
     use_env_variable: 'JAWSDB_URL',
     dialect: 'mysql'
